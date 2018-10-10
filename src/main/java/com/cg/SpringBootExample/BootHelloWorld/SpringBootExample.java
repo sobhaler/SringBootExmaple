1ddb87deb449d1e9e26f21a064571c9d8f6b928b
Your Nam <sonal.bhalerao@capgemini.com>
2018-10-10 12:36:58 +0530

    FirstSpringBoot

---
+++ src/main/java/com/cg/SpringBootExample/BootHelloWorld/SpringBootExample.java
@@ -0,0 +1,18 @@
+package com.cg.SpringBootExample.BootHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample.class, args);
+		
+	}
+		
+		
+
+	
+
+}
